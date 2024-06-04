21a318e466a10297bf18b99ce889d2a6f0f852ac
Risang Baskoro <contact@risangbaskoro.com>
2024-06-04 17:15:16 +0700

    --wip-- [skip ci] rename variables in train.py

--- train.py
+++ train.py
@@ -96,13 +96,12 @@ def train(args):
             optimizer.zero_grad()
 
             # Define input and target from the batch
-            X, y = batch
-            X = X.type(torch.float)
-            y = y.type(torch.float)
-            X, y = X.to(config.device), y.to(config.device)
+            images, targets = batch
+            images = images.type(torch.float).to(config.device)
+            targets = targets.type(torch.float).to(config.device)
 
             # Get logits
-            logits = model(X)
+            logits = model(images)
 
             # Prepare logits to calculate loss
             logits = logits.mean(dim=2)
@@ -112,14 +111,16 @@ def train(args):
             input_lengths = torch.full(size=(sample_batch_size,), fill_value=sequence_length, dtype=torch.long)
 
             # Calculate target length for each target sample
-            target_lengths = y.ne(0).sum(dim=1)
+            target_lengths = targets.ne(0).sum(dim=1)
 
             # Transpose the logits
             logits = logits.permute(2, 0, 1)  # (Timestep, Batch Size, Number of Classes)
 
+            log_probs = F.log_softmax(logits, dim=-1)
+
             # Calculate loss
-            loss = loss_fn(log_probs=F.log_softmax(logits, dim=-1),
-                           targets=y,
+            loss = loss_fn(log_probs=log_probs,
+                           targets=targets,
                            input_lengths=input_lengths,
                            target_lengths=target_lengths)
 
